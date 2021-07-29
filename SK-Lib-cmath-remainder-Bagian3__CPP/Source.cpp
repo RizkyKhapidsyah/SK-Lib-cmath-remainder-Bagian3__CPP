3eb7827b5463dbb5c9a755c230d75330397c3b74
Rizky Khapidsyah <rizkykhapidsyah@gmail.com>
2021-07-29 21:12:38 +0700

    Add project files.

---
+++ SK-Lib-cmath-remainder-Bagian3__CPP/Source.cpp
@@ -0,0 +1,23 @@
+
+#include <cmath>
+#include <iostream>
+#include <conio.h>
+
+/*
+	Source by GeeksForGeeks
+	Modified For Learn by RK
+	I.D.E : VS2019
+*/
+
+using namespace std;
+
+int main() {
+	int a = 78;
+	double b = 32.91, hasil;
+
+	hasil = remainder(a, b);
+	cout << "Remainder of " << a << "/" << b << " = " << hasil << endl;
+
+	_getch();
+	return 0;
+}
